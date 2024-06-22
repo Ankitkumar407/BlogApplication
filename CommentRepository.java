dce064c71458068a7af18c1724ef6879dd5495a6
Ankit Kumar <161424786+Ankitkumar407@users.noreply.github.com>
2024-06-22 20:53:11 +0530

    Add files via upload

---
+++ CommentRepository.java
@@ -0,0 +1,11 @@
+package com.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blog.entity.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment ,Integer> {
+	
+
+	
+}
